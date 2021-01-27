00023b9e9daae4d7a132ee01bf415cd1d945c81b
coin <wangzhenglai@zhongan.io>
2021-01-27 17:11:16 +0800

    feat free

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,19 @@
-const config = require('../config.json');
+// const config = require('@/config.json');
 module.exports = {
-  title: config.title,
-  description: config.description,
+  title: 'free',
+  description: 'free road',
   base: "/free/",
-  themeConfig: {
-    logo: config.logo,
-    footer: config.footer,
-    nav: config.navigation,
-  },
-  head: [
-    ['link', { rel: "icon", href: config.favicon }]
-  ],
-  markdown: {
-    anchor: {
-      permalink: false
-    }
-  }
+  // themeConfig: {
+  //   logo: config.logo,
+  //   footer: config.footer,
+  //   nav: config.navigation,
+  // },
+  // head: [
+  //   ['link', { rel: "icon", href: config.favicon }]
+  // ],
+  // markdown: {
+  //   anchor: {
+  //     permalink: false
+  //   }
+  // }
 };
