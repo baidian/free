5790349929be4dae3ea918bda8abdecd426425aa
coin <wangzhenglai@zhongan.io>
2021-01-28 11:21:11 +0800

    feat myj

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,19 @@
+const config = require('../config.json');
 module.exports = {
-  title: "My Free",
-  description: "This is my free.",
-  base: '/free/' 
-}
+  title: config.title,
+  description: config.description,
+  base: "/",
+  themeConfig: {
+    logo: config.logo,
+    footer: config.footer,
+    nav: config.navigation,
+  },
+  head: [
+    ['link', { rel: "icon", href: config.favicon }]
+  ],
+  markdown: {
+    anchor: {
+      permalink: false
+    }
+  }
+};
